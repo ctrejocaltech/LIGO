67d277530a7caf018952bd92eb81e458aea1b508
ctrejocaltech <139184048+ctrejocaltech@users.noreply.github.com>
2024-06-05 11:19:44 -0700

    New work with Aaron

--- LISA/kl.py
+++ LISA/kl.py
@@ -1,6 +1,5 @@
 import numpy as np
 from scipy.stats import entropy
-from scipy.special import rel_entr
 
 # Load the chains from the results files
 with open('/Users/ligo/Documents/lisa/petra/results/fake_gaussian_results_no_guess/relabeled_chains/sorted_fake_gaussian_posterior_31.npy', 'rb') as fp:
@@ -10,14 +9,19 @@ with open('/Users/ligo/Documents/lisa/petra/results/fake_gaussian_results_no_gue
     simulated_chains = np.load(fp)
 
 # Check the shape of the loaded chains
-print(f"Shape of sorted_chains: {sorted_chains.shape}")
-print(f"Shape of simulated_chains: {simulated_chains.shape}")
+#print(f"Shape of sorted_chains: {sorted_chains.shape}")
+#print(f"Shape of simulated_chains: {simulated_chains.shape}")
 
 mean_sorted = np.mean(sorted_chains, axis=0)
 mean_simulated = np.mean(simulated_chains, axis=0)
 
 cov_sorted = [np.cov(sorted_chains [:,i,:].T) for i in range(2)]
+cov_sim = [np.cov(simulated_chains [:,i,:].T) for i in range(2)]
 print(cov_sorted)
+print(cov_sim)
+
+
+
 
 sorted_chains = sorted_chains.flatten()
 simulated_chains = simulated_chains.flatten()
