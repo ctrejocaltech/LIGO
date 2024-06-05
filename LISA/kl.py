0be5f4be949a287fab0bbc2481b06e5f301bd937
ctrejocaltech <139184048+ctrejocaltech@users.noreply.github.com>
2024-06-05 11:51:16 -0700

    kl fun with aaron

--- LISA/kl.py
+++ LISA/kl.py
@@ -1,38 +1,41 @@
 import numpy as np
 from scipy.stats import entropy
 
-# Load the chains from the results files
-with open('/Users/ligo/Documents/lisa/petra/results/fake_gaussian_results_no_guess/relabeled_chains/sorted_fake_gaussian_posterior_31.npy', 'rb') as fp:
-    sorted_chains = np.load(fp)
-
-with open('/Users/ligo/Documents/lisa/petra/results/fake_gaussian_results_no_guess/simulated_chains/fake_gaussian_posterior_31.npy', 'rb') as fp:
-    simulated_chains = np.load(fp)
-
-# Check the shape of the loaded chains
-#print(f"Shape of sorted_chains: {sorted_chains.shape}")
-#print(f"Shape of simulated_chains: {simulated_chains.shape}")
-
-mean_sorted = np.mean(sorted_chains, axis=0)
-mean_simulated = np.mean(simulated_chains, axis=0)
-
-cov_sorted = [np.cov(sorted_chains [:,i,:].T) for i in range(2)]
-cov_sim = [np.cov(simulated_chains [:,i,:].T) for i in range(2)]
-print(cov_sorted)
-print(cov_sim)
+def kl_div(mu1, mu2, cov1, cov2):
+    cov2_inv = np.linalg.inv(cov2)
+    return 0.5 * (np.trace(np.dot(cov2_inv, cov1)) + np.dot(np.dot((mu2 - mu1).T, cov2_inv), (mu2 - mu1)) - 2 + np.log(np.linalg.det(cov2) / np.linalg.det(cov1)))
 
 
+def kl_function(simulated_filepath, sorted_filepath):
+    with open(simulated_filepath, 'rb') as fp:
+        simulated_chains = np.load(fp)
+    with open(sorted_filepath, 'rb') as fp:
+        sorted_chains = np.load(fp)
 
+    mean_simulated = np.mean(simulated_chains, axis=0)
+    mean_sorted = np.mean(sorted_chains, axis=0)
 
-sorted_chains = sorted_chains.flatten()
-simulated_chains = simulated_chains.flatten()
+    cov_sim = [np.cov(simulated_chains [:,i,:].T) for i in range(2)]
+    cov_sorted = [np.cov(sorted_chains [:,i,:].T) for i in range(2)]    
 
-bins = np.linspace(min(sorted_chains.min(), simulated_chains.min()), max(sorted_chains.max(), simulated_chains.max()), 100)
-hist_sorted, _ = np.histogram(sorted_chains, bins=bins, density=True)
-hist_simulated, _ = np.histogram(simulated_chains, bins=bins, density=True)
+    mu1 = mean_simulated[0, :]
+    cov1 = cov_sim[0]
 
-hist_sorted = hist_sorted / hist_sorted.sum()
-hist_simulated = hist_simulated / hist_simulated.sum()
+    mu2 = mean_sorted[0, :]
+    cov2 = cov_sorted[0]
 
-kl_divergence = entropy(hist_simulated, hist_sorted)
+    kl_list = [kl_div(mu1, mu2, cov1, cov2)]
+    mu1 = mean_simulated[1, :]
+    cov1 = cov_sim[1]
 
-print(f"KL Divergence: {kl_divergence}")
+    mu2 = mean_sorted[1, :]
+    cov2 = cov_sorted[1]
+
+    kl_list.append(kl_div(mu1, mu2, cov1, cov2))
+
+    return(kl_list)
+
+sorted_filepath = '/Users/ligo/Documents/lisa/petra/results/fake_gaussian_results_no_guess/relabeled_chains/sorted_fake_gaussian_posterior_10.npy'
+simulated_filepath = '/Users/ligo/Documents/lisa/petra/results/fake_gaussian_results_no_guess/simulated_chains/fake_gaussian_posterior_10.npy'
+
+print(kl_function(simulated_filepath, sorted_filepath))
